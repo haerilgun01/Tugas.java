9fdd601eb44222db8a65ce65b53d3a5ab13d6afc
haeril gunadi <144421938+haerilgun01@users.noreply.github.com>
2023-11-06 21:51:19 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,32 @@
+// Method .pop //
+let number = [1, 2, 3, 4, 5];
+let removeArrayNumber = number.pop();
+document.write(number);
+document.write("<br>");
+document.write("<br>");
+
+// Method .push//
+let person = ['Budi', 'Rita', 'Bayu'];
+person.push('Shinta', 'Jojo');
+document.write(person);
+document.write("<br>");
+document.write("<br>");
+
+let pers = ['Budi', 'Rita', 'Bayu'];
+pers.shift();
+console.log(pers);
+document.write(pers);
+document.write("<br>");
+document.write("<br>");
+
+let age = [17, 23, 10];
+age.unshift(20);
+console.log(age);
+document.write(age);
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+
